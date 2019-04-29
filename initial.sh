f9f944c6b8d6be75a25474d539c5c6040ce15cff
twoyang0917 <twoyang_0917@qq.com>
2019-04-29 17:04:56 +0800

    add aria2ui/emby/handbrake

--- initial.sh
+++ initial.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo yum install wget vim
+sudo yum install wget vim git
 
 curl -fsSL https://get.docker.com | bash
 sudo usermod -aG docker centos
@@ -11,6 +11,9 @@ sudo mkdir -p /docker/portainer/data
 sudo mkdir -p /docker/heimdall/config
 sudo mkdir -p /docker/minio/data
 sudo mkdir -p /docker/plex/{config,tvshows,movies,transcode}
+sudo mkdir -p /docker/handbrake/{config,watch,output}
+sudo mkdir -p /docker/emby/config
+sudo mkdir -p /docker/aria2ui/data
 sudo chown centos:centos -R /docker
 
 sudo docker run -d --name portainer -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /docker/portainer/data:/data portainer/portainer
